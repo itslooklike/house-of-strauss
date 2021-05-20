980108611492ddfff9bf06182b184d99798ddeda
a.masalov <delsol@bk.ru>
2021-05-20 10:24:35 +0300

    init app

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16-alpine
+COPY package.json yarn.lock ./
+RUN yarn
+COPY . .
+RUN yarn build
+CMD [ "yarn", "start" ]
---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import 'normalize.css'
+import './globals.css'
+
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
