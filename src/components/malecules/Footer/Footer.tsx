e55aba0848d69dc1c3324a37a47066a9ac8129d3
amasalov <delsol@bk.ru>
2021-05-22 21:02:27 +0300

    footer media

--- src/components/malecules/Footer/Footer.tsx
+++ src/components/malecules/Footer/Footer.tsx
@@ -9,11 +9,19 @@ const Inner = styled.div`
   display: grid;
   grid-gap: 40px 30px;
   grid-template-columns: 250px auto 1fr;
+  box-sizing: border-box;
   max-width: 1360px;
   margin-right: auto;
   margin-left: auto;
   padding: 60px 20px 60px;
   background: url('/images/violin-contur.svg') top 60px right 20px / 167px 190px no-repeat;
+
+  @media (max-width: 1440px) {
+    grid-gap: 0 30px;
+    grid-template-columns: auto 1fr;
+    background-position: bottom 30px right 40px;
+    background-size: 122px 138px;
+  }
 `
 
 const Info = styled.div`
@@ -21,6 +29,10 @@ const Info = styled.div`
   gap: 10px;
   align-self: start;
   max-width: 170px;
+
+  @media (max-width: 1440px) {
+    grid-column: span 2;
+  }
 `
 
 const TextMain = styled.p`
@@ -28,6 +40,18 @@ const TextMain = styled.p`
   color: var(--light-text);
   font-weight: 600;
   font-size: 14px;
+`
+
+const TextMainLink = styled.a`
+  margin: 0;
+  color: var(--light-text);
+  font-weight: 600;
+  font-size: 14px;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `
 
 const TextLight = styled.p`
@@ -41,7 +65,48 @@ const List = styled.ul`
   gap: 10px;
   margin: 0;
   padding: 0;
+  list-style: none;
+
+  @media (max-width: 1440px) {
+    margin-top: 60px;
+    margin-bottom: 60px;
+  }
 `
+
+const ListLink = styled.a`
+  color: var(--light-text);
+  font-weight: 600;
+  font-size: 14px;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const BottomSlot = styled.div`
+  @media (max-width: 1440px) {
+    grid-column: span 2;
+  }
+`
+
+const LIST_1 = [
+  { title: 'Tickets', url: '#' },
+  { title: 'Museum', url: '#' },
+  { title: 'Concerts', url: '#' },
+  { title: 'Brasserie', url: '#' },
+  { title: 'Shop', url: '#' },
+  { title: 'Directions', url: '#' },
+]
+
+const LIST_2 = [
+  { title: 'Events', url: '#' },
+  { title: 'Research', url: '#' },
+  { title: 'Masterclasses', url: '#' },
+  { title: 'History', url: '#' },
+  { title: 'About', url: '#' },
+  { title: 'Contact', url: '#' },
+]
 
 export const Footer = () => (
   <Root>
@@ -55,26 +120,30 @@ export const Footer = () => (
       </Info>
       <List>
         <TextLight>Visit</TextLight>
-        <TextMain>Tickets</TextMain>
-        <TextMain>Museum</TextMain>
-        <TextMain>Concerts</TextMain>
-        <TextMain>Brasserie</TextMain>
-        <TextMain>Shop</TextMain>
-        <TextMain>Directions</TextMain>
+        {LIST_1.map((item) => (
+          <li>
+            <ListLink href={item.url}>{item.title}</ListLink>
+          </li>
+        ))}
       </List>
       <List>
         <TextLight>More</TextLight>
-        <TextMain>Events</TextMain>
-        <TextMain>Research</TextMain>
-        <TextMain>Masterclasses</TextMain>
-        <TextMain>History</TextMain>
-        <TextMain>About</TextMain>
-        <TextMain>Contact</TextMain>
+        {LIST_2.map((item) => (
+          <li>
+            <ListLink href={item.url}>{item.title}</ListLink>
+          </li>
+        ))}
       </List>
 
-      <TextLight>© House of Strauss</TextLight>
-      <TextMain>Imprint</TextMain>
-      <TextMain>Privacy Policy</TextMain>
+      <BottomSlot>
+        <TextLight>© House of Strauss</TextLight>
+      </BottomSlot>
+      <BottomSlot>
+        <TextMainLink href="#">Imprint</TextMainLink>
+      </BottomSlot>
+      <BottomSlot>
+        <TextMainLink href="#">Privacy Policy</TextMainLink>
+      </BottomSlot>
     </Inner>
   </Root>
 )
