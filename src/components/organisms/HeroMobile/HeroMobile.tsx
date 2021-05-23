bf36590fb4471772490a5f11f84e39cca34efa62
a.masalov <delsol@bk.ru>
2021-05-23 21:19:36 +0300

    mobile hero

---
+++ src/components/organisms/HeroMobile/HeroMobile.tsx
@@ -0,0 +1,25 @@
+import cx from 'classnames'
+import { ButtonGhost } from '~/components/atoms'
+
+import css from './HeroMobile.module.scss'
+
+type TProps = {
+  title: string
+  text: string
+  link: string
+  type: 'var1' | 'var2' | 'var3'
+}
+
+export const HeroMobile = (props: TProps) => {
+  const { title, text, link, type } = props
+
+  return (
+    <div className={cx(css.root, css[type])}>
+      <div className={css.content}>
+        <div className={css.title}>{title}</div>
+        <div className={css.text}>{text}</div>
+        <ButtonGhost>{link}</ButtonGhost>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import type { NextPage } from 'next'
 import { CardSmall, CardLarge, CardMedium } from '~/components/atoms'
 import { Footer, InnerMap } from '~/components/molecules'
-import { Hero } from '~/components/organisms'
+import { Hero, HeroMobile } from '~/components/organisms'
 import css from './index.module.scss'
 
 const Home: NextPage = () => (
   <div>
-    <Hero />
+    <div className={css.heroWrapDesktop}>
+      <Hero />
+    </div>
+    <div className={css.heroWrapMobile}>
+      <HeroMobile
+        type="var1"
+        title="Museum"
+        text="Eintauchen in die Strauss Dynastie"
+        link="More"
+      />
+      <HeroMobile
+        type="var2"
+        title="Concerts"
+        text="A musical evening in Vienna"
+        link="Coming soon"
+      />
+      <HeroMobile
+        type="var3"
+        title="Restaurant"
+        text="A musical evening in Vienna"
+        link="Reservation"
+      />
+    </div>
     <div className={css.mainBlock}>
       <div className={css.contentTitle}>
         House of Strauss offers a unique musical and culinary experience in Vienna, the Capital of
