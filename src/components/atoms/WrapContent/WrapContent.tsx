745aaac1662d18c03da0835ca9f7cce230999ba5
a.masalov <delsol@bk.ru>
2021-05-25 20:23:52 +0300

    privacy pages

---
+++ src/components/atoms/WrapContent/WrapContent.tsx
@@ -0,0 +1,3 @@
+import css from './WrapContent.module.scss'
+
+export const WrapContent: React.FC = ({ children }) => <div className={css.root}>{children}</div>
---
+++ src/components/atoms/WrapContent/index.tsx
@@ -0,0 +1,1 @@
+export { WrapContent } from './WrapContent'
