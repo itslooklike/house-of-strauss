cfec64fb4c21204dfb0b9d8a2127816d1275ac2d
a.masalov <delsol@bk.ru>
2021-05-26 13:39:00 +0300

    add validation

--- src/components/molecules/FormDefault/FormDefault.tsx
+++ src/components/molecules/FormDefault/FormDefault.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
+import { useFormik } from 'formik'
 import { Input, ButtonForm } from '~/components/atoms'
 import { IconCheck } from '~/icons'
 import { allRoutes } from '~/utils/config'
+import { validate } from '~/utils/validations'
 import css from './FormDefault.module.scss'
 
 type TProps = {
@@ -9,29 +11,58 @@ type TProps = {
   mobileLight?: boolean
 }
 
-export const FormDefault = ({ title, mobileLight }: TProps) => (
-  <form>
-    {title && <p className={css.title}>{title}</p>}
+export const FormDefault = ({ title, mobileLight }: TProps) => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
 
-    <div className={css.firstInput}>
-      <Input placeholder="Full name" type="text" mobileLight={mobileLight} />
-    </div>
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      {title && <p className={css.title}>{title}</p>}
 
-    <div className={css.secondInput}>
-      <Input placeholder="Email" type="email" mobileLight={mobileLight} />
-    </div>
+      <div className={css.firstInput}>
+        <Input
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          placeholder="Full name"
+          type="text"
+          mobileLight={mobileLight}
+        />
+        {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      </div>
 
-    <div className={css.controlWrap}>
-      <ButtonForm type="submit" mobileLight={mobileLight}>
-        <IconCheck width={20} />
-        &nbsp;Submit
-      </ButtonForm>
-      <span className={css.policy}>
-        By submitting you accept our{' '}
-        <Link href={allRoutes.privacy.url}>
-          <a className={css.policyLink}>privacy policy</a>
-        </Link>
-      </span>
-    </div>
-  </form>
-)
+      <div className={css.secondInput}>
+        <Input
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          placeholder="Email"
+          type="email"
+          mobileLight={mobileLight}
+        />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+      </div>
+
+      <div className={css.controlWrap}>
+        <ButtonForm type="submit" mobileLight={mobileLight}>
+          <IconCheck width={20} />
+          &nbsp;Submit
+        </ButtonForm>
+        <span className={css.policy}>
+          By submitting you accept our{' '}
+          <Link href={allRoutes.privacy.url}>
+            <a className={css.policyLink}>privacy policy</a>
+          </Link>
+        </span>
+      </div>
+    </form>
+  )
+}
---
+++ src/utils/validations.ts
@@ -0,0 +1,17 @@
+type TValidate = { name?: string; email?: string }
+
+export const validate = (values: TValidate) => {
+  const errors: TValidate = {}
+
+  if (!values.name) {
+    errors.name = 'Required'
+  }
+
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  return errors
+}
