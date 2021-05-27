ebfd9d4cc923d06c5bf709a4e7d035b44f753590
amasalov <delsol@bk.ru>
2021-05-27 20:58:52 +0300

    add preload

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import cx from 'classnames'
+import Head from 'next/head'
 import { HeroTextSlot } from '~/components/molecules'
 import { allRoutes } from '~/utils/config'
 import css from './Hero.module.scss'
@@ -11,32 +12,39 @@ export const Hero = () => {
   const handleHover3 = () => setType('var3')
 
   return (
-    <div className={cx(css.hero, css[type])}>
-      <div className={css.inner}>
-        <div className={css.contentWrap}>
-          <HeroTextSlot
-            onMouseEnter={handleHover1}
-            logo="Museum"
-            text="Eintauchen in die Strauss Dynastie"
-            link="More"
-            route={allRoutes.museum}
-          />
-          <HeroTextSlot
-            onMouseEnter={handleHover2}
-            logo="Concerts"
-            text="A musical evening in Vienna"
-            link="Reservation"
-            route={allRoutes.concerts}
-          />
-          <HeroTextSlot
-            onMouseEnter={handleHover3}
-            logo="Restaurant"
-            text="A musical evening in Vienna"
-            link="Coming soon"
-            route={allRoutes.brasserie}
-          />
+    <>
+      <Head>
+        <link rel="preload" href="/images/museum-1.jpg" as="image" />
+        <link rel="preload" href="/images/museum-2.jpg" as="image" />
+        <link rel="preload" href="/images/museum-3.jpg" as="image" />
+      </Head>
+      <div className={cx(css.hero, css[type])}>
+        <div className={css.inner}>
+          <div className={css.contentWrap}>
+            <HeroTextSlot
+              onMouseEnter={handleHover1}
+              logo="Museum"
+              text="Eintauchen in die Strauss Dynastie"
+              link="More"
+              route={allRoutes.museum}
+            />
+            <HeroTextSlot
+              onMouseEnter={handleHover2}
+              logo="Concerts"
+              text="A musical evening in Vienna"
+              link="Reservation"
+              route={allRoutes.concerts}
+            />
+            <HeroTextSlot
+              onMouseEnter={handleHover3}
+              logo="Restaurant"
+              text="A musical evening in Vienna"
+              link="Coming soon"
+              route={allRoutes.brasserie}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
