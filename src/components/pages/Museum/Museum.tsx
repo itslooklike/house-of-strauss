885b422b385881fc885501aee4b92f8698f77f0c
itslooklike <delsol@bk.ru>
2021-06-13 11:42:49 +0300

    hero

--- src/components/pages/Museum/Museum.tsx
+++ src/components/pages/Museum/Museum.tsx
@@ -1,3 +1,4 @@
+import { Sponsors } from '~/components/atoms'
 import { FormDefault } from '~/components/molecules'
 import { Header } from '~/components/organisms'
 import { LWithFooter } from '~/components/layouts'
@@ -5,21 +6,27 @@ import css from './Museum.module.scss'
 
 export const Museum = () => (
   <LWithFooter>
-    <Header mobileDark />
-    <div className={css.hero} />
-    <main className={css.main}>
-      <h1 className={css.title}>
-        The King of Waltz, Johann Strauss II, was arguably the world‘s first pop star
-      </h1>
-      <div className={css.content}>
-        <div className={css.contentInner}>
-          <p className={css.subtitle}>
-            Dive into the story of the Strauss dynasty while their legacy comes alive around you.
-          </p>
-          <p className={css.text}>The museum will open in June 2022. More info will follow soon.</p>
-          <FormDefault title="Subscribe to our newsletter to stay up to date" mobileLight />
+    <Header />
+    <div className={css.hero}>Exhibition</div>
+    <main className={css.mainWrap}>
+      <div className={css.main}>
+        <div className={css.mainBlock}>
+          <h1 className={css.title}>
+            The King of Waltz, Johann Strauss II, was arguably the world‘s first pop star
+          </h1>
+          <div className={css.content}>
+            <p>
+              Dive into the story of the Strauss dynasty while their legacy comes alive around you.
+            </p>
+            <p className={css.text}>
+              The museum will open in June 2022. More info will follow soon.
+            </p>
+            <FormDefault title="Subscribe to our newsletter to stay up to date" />
+          </div>
         </div>
       </div>
     </main>
+
+    <Sponsors />
   </LWithFooter>
 )
