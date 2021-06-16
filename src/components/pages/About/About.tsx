71bbcf91f86690ce18a858f8270e87ddc02a6d0c
itslooklike <delsol@bk.ru>
2021-06-16 11:42:09 +0300

    update about

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,28 +1,45 @@
-import { WrapContent, TextPageTitle } from '~/components/atoms'
+import { WrapContent, Sponsors } from '~/components/atoms'
 import { Header } from '~/components/organisms'
 import { LWithFooter } from '~/components/layouts'
 import css from './About.module.scss'
+
+const persons = [
+  { title: 'Herman Rauter', description: 'Initiator', imgUrl: '/images/image-about.jpg' },
+  { title: 'Herman Rauter', description: 'Initiator', imgUrl: '/images/image-about.jpg' },
+  { title: 'Herman Rauter', description: 'Initiator', imgUrl: '/images/image-about.jpg' },
+  { title: 'Herman Rauter', description: 'Initiator', imgUrl: '/images/image-about.jpg' },
+  { title: 'Herman Rauter', description: 'Initiator', imgUrl: '/images/image-about.jpg' },
+]
 
 export const About = () => (
   <LWithFooter>
     <Header type="dark" />
     <WrapContent>
+      <h1 className={css.title}>About</h1>
+
       <div className={css.content}>
-        <div className={css.col1}>
-          <TextPageTitle>About</TextPageTitle>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-        </div>
-        <div className={css.col2}>
-          <img width="400" src="/images/image-about.jpg" alt="author" />
-        </div>
+        <p className={css.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+
+        <p className={css.projectTitle}>Projektteam</p>
+      </div>
+
+      <div className={css.aboutWrap}>
+        {persons.map((person) => (
+          <div>
+            <img width="200" src={person.imgUrl} alt={person.title} />
+            <div>{person.title}</div>
+            <div>{person.description}</div>
+          </div>
+        ))}
       </div>
     </WrapContent>
+    <Sponsors />
   </LWithFooter>
 )
